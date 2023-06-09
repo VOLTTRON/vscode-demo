e827534a283297d43a923710c98d50901be30606
Craig <3979063+craig8@users.noreply.github.com>
2023-06-09 09:44:50 -0700

    Update modular-dev-setup.sh

--- scripts/modular-dev-setup.sh
+++ scripts/modular-dev-setup.sh
@@ -1,4 +1,15 @@
-sudo apt install python3-dev
+sudo apt update
+sudo apt install -y python3-dev python3-venv 
+
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+
+pipx completions
+
+echo eval "$(register-python-argcomplete pipx)" >> ~/.bashrc
+
+
+pipx install copier
 
 curl -sSL https://install.python-poetry.org | python3 -
 
